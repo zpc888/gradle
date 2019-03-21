71c297ccebd475ad5f741c014ddedfb1f7d9c239
Daz DeBoer <daz@gradle.com>
2019-03-21 12:04:16 -0600

    Add GE configuration for testing on Jenkins

--- build.gradle.kts
+++ build.gradle.kts
@@ -386,3 +386,19 @@ tasks.register<Zip>("allIncubationReportsZip") {
 }
 
 fun Project.collectAllIncubationReports() = subprojects.flatMap { it.tasks.withType(IncubatingApiReportTask::class) }
+
+// Ensure the archives produced are reproducible
+allprojects {
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+        dirMode = Integer.parseInt("0755", 8)
+        fileMode = Integer.parseInt("0644", 8)
+    }
+}
+
+buildScan {
+    server = System.getenv("GE_URL")
+    isCaptureTaskInputFiles = true
+    publishAlways()
+}
